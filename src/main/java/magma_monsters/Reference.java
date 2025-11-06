fdf9058d33cf5e06481889a55e2e34edd3972bb6
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-11-06 14:43:26 +0000

    Start of port crap... again

--- src/main/java/magma_monsters/Reference.java
+++ src/main/java/magma_monsters/Reference.java
@@ -2,6 +2,6 @@ package magma_monsters;
 
 public class Reference {
 	public static final String MOD_ID = "magma_monsters";
-	public static final String MOD_NAME = "magma_monsters";
-	public static final String VERSION = "1.0.0";
+	public static final String MOD_NAME = "Magma Monsters";
+	public static final String VERSION = "2.0.0";
 }
